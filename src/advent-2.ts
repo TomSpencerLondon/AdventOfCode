d3a9bed45299a3ef9c7a6426a6ae23c910b89226
Tom <thomas.spencer@codurance.com>
2020-12-04 09:33:39 +0000

    Add code for day 2

--- src/advent-2.ts
+++ src/advent-2.ts
@@ -43,3 +43,47 @@ export const countPasswords = async (filename: string): Promise<number> => {
 
   return finalResult;
 };
+
+export const countOnePositionPasswords = async (
+  filename: string
+): Promise<number> => {
+  const content: Promise<Buffer> = promisify(fs.readFile)(filename);
+
+  const result = await content;
+  const lines = result.toString().split("\n");
+  const list: string[] = [];
+  const allowedLetters: string[] = [];
+  const passwords: string[] = [];
+  for (let i = 0; i < lines.length; i++) {
+    list.push(lines[i].substring(0, lines[i].indexOf(" ")));
+  }
+
+  for (let i = 0; i < lines.length; i++) {
+    allowedLetters.push(lines[i].replace(/:/g, "").split(" ")[1]);
+  }
+
+  for (let i = 0; i < lines.length; i++) {
+    passwords.push(lines[i].replace(/:/g, "").split(" ")[2]);
+  }
+
+  const firstNumbers: string[] = [];
+  const secondNumbers: string[] = [];
+  for (let i = 0; i < list.length - 1; i++) {
+    const numbers = list[i].split("-");
+    firstNumbers.push(numbers[0]);
+    secondNumbers.push(numbers[1]);
+  }
+
+  let finalResult = 0;
+  for (let i = 0; i < lines.length - 1; i++) {
+    const letters = passwords[i];
+    if (
+      (letters[parseInt(firstNumbers[i]) - 1] === allowedLetters[i]) !==
+      (letters[parseInt(secondNumbers[i]) - 1] === allowedLetters[i])
+    ) {
+      finalResult += 1;
+    }
+  }
+
+  return finalResult;
+};
--- test/advent-2.test.ts
+++ test/advent-2.test.ts
@@ -1,8 +1,13 @@
-import { countPasswords } from "../src/advent-2";
+import { countOnePositionPasswords, countPasswords } from "../src/advent-2";
 
 describe("day 2", () => {
   it("counts valid passwords in list from numbers on left and letter", async () => {
     const result = await countPasswords("./test/data/day_2.txt");
     expect(result).toEqual(572);
+  });
+
+  it("counts valid single character occurrence password criteria from numbers on left and letter", async () => {
+    const result = await countOnePositionPasswords("./test/data/day_2.txt");
+    expect(result).toEqual(454);
   });
 });
