2a3736cce59ff629d1ec2b0dc56aaaa7d4b67017
Tom <thomas.spencer@codurance.com>
2020-12-11 00:01:47 +0000

    Add expected

--- test/advent-3.test.ts
+++ test/advent-3.test.ts
@@ -3,6 +3,6 @@ import { countTrees } from "../src/advent-3";
 describe("day 3", () => {
   it("counts trees encountered by toboggan", async () => {
     const result = await countTrees("./test/data/day_3.txt");
-    expect(result).toEqual(454);
+    expect(result).toEqual(7);
   });
 });
