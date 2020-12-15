9a0a7d1fe59bea46d665f4aeb2918d85664c7f81
Tom <thomas.spencer@codurance.com>
2020-12-15 09:24:28 +0000

    Refactor for clarity on hasAllFields

--- src/advent-4.ts
+++ src/advent-4.ts
@@ -3,23 +3,24 @@
 
 import { promises as fs } from "fs";
 
+function hasAllFields(
+  expectedFields: string[],
+  actualFields: string[]
+): boolean {
+  return expectedFields.every((field) => actualFields.includes(field));
+}
+
 export const part1 = async (filename: string): Promise<number> => {
   const result = await readFile(filename);
   const passports = result.toString().split("\n\n");
-  const strippedFields = passports.map((p) =>
-    p
-      .split(/\s/)
-      .filter((p) => p)
-      .map((p) => p.split(":")[0])
+  const strippedFields = passports.map((passport) =>
+    passport.split(/\s/).map((field) => field.split(":")[0])
   );
   const expectedFields = ["ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt"];
 
-  const number = strippedFields.reduce((acc, s) => {
-    const every = +expectedFields.every((f) => {
-      const includes = s.includes(f);
-      return includes;
-    });
-    return acc + every;
+  const number = strippedFields.reduce((acc, array) => {
+    const result = hasAllFields(expectedFields, array) ? 1 : 0;
+    return acc + result;
   }, 0);
 
   return number;
@@ -38,15 +39,14 @@ export const part2 = async (filename: string): Promise<number> => {
       });
     return result;
   });
-  const expectedFields = ["ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt"];
 
-  const number = passportObjects.reduce((acc, s) => {
-    const every = +expectedFields.every((f) => {
-      const includes = s[f].includes(f);
-      return includes;
-    });
-    return acc + every;
-  }, 0);
+  let number = 0;
+
+  passportObjects.forEach((p) => {
+    if (isValid(p)) {
+      number++;
+    }
+  });
 
   return number;
 };
@@ -67,26 +67,75 @@ const isValid = (passport: { [key: string]: string }): boolean => {
   // byr (Birth Year) - four digits; at least 1920 and at most 2002.
   // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
   // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+
   // hgt (Height) - a number followed by either cm or in:
   // endsWith() - javascript
   // If cm, the number must be at least 150 and at most 193.
   // startsWith() - javascript
+
   // If in, the number must be at least 59 and at most 76.
   // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
   // includes() / sum()
   // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+
   // pid (Passport ID) - a nine-digit number, including leading zeroes.
   // cid (Country ID) - ignored, missing or not.
 
-  if (passport["byr"].length !== 4) {
+  if (
+    passport["byr"].length !== 4 ||
+    passport["iyr"].length !== 4 ||
+    passport["eyr"].length !== 4
+  ) {
     return false;
   }
 
-  if (+passport["byr"] < 1920 || +passport["byr"] > 2002) {
+  if (
+    +passport["byr"] < 1920 ||
+    +passport["byr"] > 2002 ||
+    +passport["iyr"] < 2010 ||
+    +passport["iyr"] > 2020 ||
+    +passport["eyr"] < 2020 ||
+    +passport["eyr"] > 2030
+  ) {
     return false;
   }
 
-  throw Error("Unsupported");
+  if (!passport["hgt"].endsWith("cm") && !passport["hgt"].endsWith("in")) {
+    return false;
+  }
+
+  if (passport["hgt"].endsWith("cm")) {
+    const number = parseInt(passport["hgt"].replace(/\D/g, ""));
+    if (number < 150 || number > 193) {
+      return false;
+    }
+  } else if (passport["hgt"].endsWith("in")) {
+    const number = parseInt(passport["hgt"].replace(/\D/g, ""));
+
+    if (number < 59 || number > 76) {
+      return false;
+    }
+  }
+
+  const regex = RegExp(/^#[0-9A-F]{6}$/i);
+  if (!regex.test(passport["hcl"])) {
+    return false;
+  }
+
+  const checker = (value) =>
+    ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].some((element) =>
+      value.includes(element)
+    );
+
+  if (!checker(passport["ecl"])) {
+    return false;
+  }
+
+  const numberRegex = RegExp(/^\d+$/);
+  if (!numberRegex.test(passport["pid"]) || passport["pid"].length !== 9) {
+    return false;
+  }
+  return true;
 };
 
 async function readFile(filename: string) {
--- test/advent-4.test.ts
+++ test/advent-4.test.ts
@@ -8,6 +8,6 @@ describe("Day 4", () => {
 
   it("should validate passport details", async () => {
     const result = await part2("./test/data/day_4.txt");
-    expect(result).toEqual(2);
+    expect(result).toEqual(147);
   });
 });
