73998a82760c76efd327a91eae00a9aa342cc859
Tom <thomas.spencer@codurance.com>
2021-01-03 23:28:57 +0000

    Add day 8

--- src/advent-8.ts
+++ src/advent-8.ts
@@ -49,3 +49,87 @@ export const part1 = async (filename: string): Promise<number> => {
 
   return accumulator;
 };
+
+export const part2 = async (filename: string): Promise<number> => {
+  const lines = await read(filename);
+
+  const instructions: Instruction[] = [];
+
+  lines.forEach((line) => {
+    const [operation, argument] = line.split(" ");
+    const instruction: Instruction = {
+      operation: operation as "acc" | "jmp" | "nop",
+      argument: parseInt(argument),
+    };
+
+    instructions.push(instruction);
+  });
+
+  const { visitedInstructions } = run(instructions);
+
+  const possiblyFaultyInstructions = [
+    ...Array.from(new Set(visitedInstructions)),
+  ].filter((instruction) => ["jmp", "nop"].includes(instruction.operation));
+
+  for (const possiblyFaultyInstruction of possiblyFaultyInstructions) {
+    const initialOperation = possiblyFaultyInstruction.operation;
+
+    possiblyFaultyInstruction.operation =
+      initialOperation === "jmp" ? "nop" : "jmp";
+
+    const { exitCode, accumulator } = run(instructions);
+
+    if (exitCode === 0) {
+      return accumulator;
+    }
+
+    possiblyFaultyInstruction.operation = initialOperation;
+  }
+};
+
+
+function run(instructions: Instruction[]): RunResult {
+  // THIS IS NEW!
+  let exitCode = 0;
+
+  let accumulator = 0;
+
+  let position = 0;
+  let instruction = instructions[position];
+
+  const visitedInstructions = new Set<Instruction>();
+
+  // THIS HAS CHANGED!
+  while (instruction) {
+    // THIS IS NEW!
+    if (visitedInstructions.has(instruction)) {
+      exitCode = 1;
+      break;
+    }
+
+    switch (instruction.operation) {
+      case "acc":
+        accumulator += instruction.argument;
+        position++;
+        break;
+      case "jmp":
+        position += instruction.argument;
+        break;
+      case "nop":
+        position++;
+        break;
+    }
+
+    visitedInstructions.add(instruction);
+    instruction = instructions[position];
+  }
+
+  // THIS HAS CHANGED!
+  return { exitCode, accumulator, visitedInstructions };
+}
+
+interface RunResult {
+  exitCode: number;
+  accumulator: number;
+  visitedInstructions: Set<Instruction>;
+}
--- test/advent-8.test.ts
+++ test/advent-8.test.ts
@@ -1,8 +1,13 @@
-import { part1 } from "../src/advent-8";
+import { part1, part2 } from "../src/advent-8";
 
-describe("Gold shiny bags", () => {
+describe("Game", () => {
   it("should solve for the first example", async () => {
     const result = await part1("./test/data/day_8.txt");
     expect(result).toEqual(2014);
+  });
+
+  it("should solve for the second example", async () => {
+    const result = await part2("./test/data/day_8.txt");
+    expect(result).toEqual(2251);
   });
 });
